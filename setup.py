c9c486249ce4c02435aff9bdf317e9bd0ed77352
eliacer95 <eliacer.fernandez@upeu.edu.pe>
2017-07-12 12:14:13 -0500

    Changes

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ setup(
   author = 'Eliacer Fernandez Guevara',
   author_email = 'eliacer.fernandez@upeu.edu.pe',
   url = 'https://github.com/eliacer95/django-pack-notification', # use the URL to the github repo
-  download_url = 'https://github.com/eliacer95/django-pack-notification/tarball/0.2',
+  download_url = 'https://github.com/eliacer95/django-pack-notification/tarball/0.1',
   keywords = ['django', 'slack'],
   classifiers = [],
 )
