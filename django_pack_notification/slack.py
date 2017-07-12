8b08bab7521c2fea3297b0bdcfea108ccb4f4f4e
eliacer95 <eliacer.fernandez@upeu.edu.pe>
2017-07-12 16:48:03 -0500

    Changes...

--- django_pack_notification/slack.py
+++ django_pack_notification/slack.py
@@ -6,7 +6,9 @@ import json
 def slack_success(url, title, message, subtitle=None):
     if subtitle is None:
         subtitle = ""
-    data = {"attachments": [{"title": title, "pretext": subtitle, "text": message, "color": "#7CD197", "mrkdwn_in": ["text", "pretext"]}]}
+    data = {
+        "text": title,
+        "attachments": [{"title": subtitle, "text": message, "color": "#7CD197", "mrkdwn_in": ["text"]}]}
     r = requests.post(url, data=json.dumps(data), headers={'Content-Type': 'application/json'})
     if r.status_code == 200:
         return True
@@ -18,7 +20,10 @@ def slack_error(url, title, message, subtitle=None):
     if subtitle is None:
         subtitle = ""
     text = "```\n%s\n```" % (message)
-    data = {"attachments": [{"title": title, "pretext": subtitle, "text": text, "color": "#F02742", "mrkdwn_in": ["text", "pretext"]}]}
+    data = {
+        "text": title,
+        "attachments": [{"title": subtitle, "text": text, "color": "#F02742", "mrkdwn_in": ["text", "pretext"]}]
+    }
     r = requests.post(url, data=json.dumps(data), headers={'Content-Type': 'application/json'})
     if r.status_code == 200:
         return True
